2a8193451e9b61ea04e034064ad2f30e37506679
Grzegorz Rybkowski <rybkowski.grzegorz@gmail.com>
2019-03-24 18:31:28 +0100

    [general] removed create-react-app

--- src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import * as Styled from './Button.styled'
-
-const Button = ({ label, onClick }) => {
-  return <Styled.Container onClick={onClick}>{label}</Styled.Container>
-}
-
-export default Button
--- src/components/Button/Button.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Container = styled.button`
-  background: violet;
-  padding: 16px;
-  font-size: 20px;
-  line-height: 24px;
-`
